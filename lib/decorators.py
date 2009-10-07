bcbacee0ecb984d6b39f47ff85380218ba49c04d
Ruslan <ruslan.popov@gmail.com>
2009-10-08 00:37:53 +0400

    Remove doubles.

--- lib/decorators.py
+++ lib/decorators.py
@@ -1,14 +1,20 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-def render_to(template):
+def render_to(template, processor=None):
     def renderer(func):
         def wrapper(request, *args, **kw):
+            if processor is not None:
+                ctx_proc = RequestContext(request, processors=[processor])
+            else:
+                ctx_proc = RequestContext(request)
             output = func(request, *args, **kw)
             if isinstance(output, (list, tuple)):
-                return render_to_response(output[1], output[0], RequestContext(request))
+                return render_to_response(output[1], output[0], ctx_proc)
             elif isinstance(output, dict):
-                return render_to_response(template, output, RequestContext(request))
+                return render_to_response(template, output, ctx_proc)
             return output
         return wrapper
     return renderer
