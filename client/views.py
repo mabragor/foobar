5e68dd37c1146de3e60728014fca9404ea117901
Ruslan <ruslan.popov@gmail.com>
2009-10-09 23:26:32 +0400

    Swap client and manager directories.

--- client/views.py
+++ client/views.py
@@ -1,63 +1,10 @@
 # -*- coding: utf-8 -*-
-
-from django.conf import settings
-from lib import DatetimeJSONEncoder
-from django.utils import simplejson
-from models import Schedule, Course, Room
-from django.http import HttpResponse
-from django.utils.translation import ugettext_lazy as _
-from datetime import datetime
-from forms import ScheduleForm
 from lib.decorators import render_to
 
-@render_to('client/index.html')
+@render_to('manager.html')
 def index(request):
-    form = ScheduleForm()
-    rooms = Room.objects.all()
+
     return {
-        'form': form,
-        'rooms': simplejson.dumps([item.get_calendar_obj() for item in rooms], cls=DatetimeJSONEncoder),
-        'options': simplejson.dumps(settings.CALENDAR_OPTIONS, cls=DatetimeJSONEncoder)
     }
 
-def ajax_add_event(request, pk=None):
-    output = {}
-    if request.method == 'POST':
-        if pk:
-            event = Schedule.objects.get(pk=pk)
-            form = ScheduleForm(request.POST, instance=event)
-        else:
-            form = ScheduleForm(request.POST)
-        if form.is_valid():
-            obj = form.save()
-            output['obj'] = obj.get_calendar_obj()
-        else:
-            output['error'] = u'Form isn\'t valid.'
-    else:
-        output['error'] = u'Incorect request method.'
-    return HttpResponse(simplejson.dumps(output, cls=DatetimeJSONEncoder))
-
-def ajax_del_event(request):
-    if request.method == 'POST':
-        Schedule.objects.get(pk=request.POST['id']).delete()
-    return HttpResponse('{}')
-
-def ajax_change_date(request):
-    if request.method == 'POST':
-        event = Schedule.objects.get(pk=request.POST['pk'])
-        event.begin = datetime.fromtimestamp(int(request.POST['start']))
-        event.save()
-    return HttpResponse('{}')
-
-def ajax_get_events(request):
-    if request.method == 'POST':
-        start = datetime.fromtimestamp(int(request.POST['start']))
-        end = datetime.fromtimestamp(int(request.POST['end']))
-        schedules = Schedule.objects.filter(begin__range=(start, end))
-        if 'rooms' in request.POST:
-            schedules = schedules.filter(room__in=request.POST['rooms'])
-        events = [item.get_calendar_obj() for item in schedules]
-    else:
-        events = []
-    return HttpResponse(simplejson.dumps(events, cls=DatetimeJSONEncoder))
 
--- manager/urls.py
+++ manager/urls.py
@@ -2,7 +2,11 @@
 
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('manager.views',
+urlpatterns = patterns('client.views',
     url(r'^$', 'index'),
+    url(r'^add_event/$', 'ajax_add_event', name='ajax_add_event'),
+    url(r'^add_event/(?P<pk>\d+)/$', 'ajax_add_event'),
+    url(r'^del_event/$', 'ajax_del_event', name='ajax_del_event'),
+    url(r'^change_date/$', 'ajax_change_date', name='ajax_change_date'),
+    url(r'^get_events/$', 'ajax_get_events', name='ajax_get_events')
 )
-
