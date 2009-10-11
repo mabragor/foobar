7801ec3381b5c12091d6c54ee670a1ec9867c523
Ruslan <ruslan.popov@gmail.com>
2009-10-11 22:16:55 +0400

    Make form code more clear.

--- js/foobar-manager.js
+++ js/foobar-manager.js
@@ -3,36 +3,23 @@ Ext.BLANK_IMAGE_URL = './media/ext/resources/default/s.gif';
 Ext.onReady(function() {
     Ext.QuickTips.init();
 
+    // This form shows information about current client. Manager may check 
+    // client's data, add new courses to client account, take subcharge 
+    // from him.
     var client_form = new Ext.form.FormPanel({
 	standardSubmit: true,
 	labelWidth: 100,
 	frame: false,
 	items:[
-	    new Ext.form.TextField({
-		name: 'first_name',
-		fieldLabel: 'First name',
-		allowBlank: false
-	    }), 
-	    new Ext.form.TextField({
-		name: 'last_name',
-		fieldLabel: 'Last name',
-		allowBlank: false
-	    }),
-	    new Ext.form.TextField({
-		name: 'email',
-		fieldLabel: 'E-mail',
-		allowBlank: false
-	    })],
-	buttons: [{
-	    text: 'Surchange',
-	    handler: function() {}
-	}, {
-	    text: 'Apply',
-	    handler: function() {}
-	}, {
-	    text: 'Cancel',
-	    handler: function() {}
-	}]
+	    new Ext.form.TextField({ name: 'first_name', fieldLabel: 'First name', allowBlank: false }), 
+	    new Ext.form.TextField({ name: 'last_name', fieldLabel: 'Last name', allowBlank: false }),
+	    new Ext.form.TextField({ name: 'email', fieldLabel: 'E-mail', allowBlank: false })
+	],
+	buttons: [
+	    { text: 'Surchange', handler: function() {} }, 
+	    { text: 'Apply', handler: function() {} }, 
+	    { text: 'Cancel', handler: function() {} }
+	]
     });
 
     var panel = new Ext.Viewport({
