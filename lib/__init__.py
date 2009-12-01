cff807396c446611e69c619e396f75f0869d40a1
Ruslan <ruslan.popov@gmail.com>
2009-12-02 00:16:21 +0300

    Addition JSON encoder for Qt.

--- lib/__init__.py
+++ lib/__init__.py
@@ -6,7 +6,7 @@ class DatetimeJSONEncoder(simplejson.JSONEncoder):
     class DateInt(int):
         def __str__(self):
             return 'new Date(%s)' % (self*1000)
-        
+
     def default(self, o):
         from django.utils import datetime_safe
         from datetime import datetime
@@ -15,5 +15,19 @@ class DatetimeJSONEncoder(simplejson.JSONEncoder):
         if isinstance(o, datetime):
             d = datetime_safe.new_datetime(o)
             return self.DateInt(int(time.mktime(d.timetuple())))
+        else:
+            return super(DatetimeJSONEncoder, self).default(o)
+
+class DatetimeJSONEncoderQt(simplejson.JSONEncoder):
+
+    def default(self, o):
+        from django.utils import datetime_safe
+        from datetime import datetime
+        import time
+
+        if isinstance(o, datetime):
+            d = o.strftime('%Y-%m-%d %H:%M:%S')
+            print d, type(d)
+            return d
         else:
             return super(DatetimeJSONEncoder, self).default(o)
