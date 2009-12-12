8e84370b0f5158bf7af581558bd66bbafc0f30e5
Ruslan <ruslan.popov@gmail.com>
2009-12-12 23:39:27 +0300

    Proxy models storage.

---
+++ qt/model_sorting.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# (c) 2009 Ruslan Popov <ruslan.popov@gmail.com>
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+class SortClientCourses(QSortFilterProxyModel):
+
+    """ Proxy class to change representation of client's courses
+    information. """
+
+    def __init__(self, parent=None):
+        QSortFilterProxyModel.__init__(self, parent)
