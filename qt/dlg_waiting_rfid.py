0a0df7e4a2eace4bf5709fbaaabe22057c5f0e8c
Ruslan <ruslan.popov@gmail.com>
2009-12-12 23:39:59 +0300

    Refactoring: Move WaitingRFID into appropriate dialog module.

--- qt/dlg_waiting_rfid.py
+++ qt/dlg_waiting_rfid.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 # (c) 2009 Ruslan Popov <ruslan.popov@gmail.com>
 
+import serial, random, time
+
+from settings import DEBUG, PORT
+
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
-
-from rfid import WaitingRFID
 
 class DlgWaitingRFID(QDialog):
 
@@ -40,3 +42,85 @@ class DlgWaitingRFID(QDialog):
         self.reader.timeToDie()
         # закрываем диалог
         self.reject()
+
+class WaitingRFID(QThread):
+    def __init__(self, parent):
+        self.dialog = parent.dialog
+        self.callback = parent.callback
+
+        self.disposed = False
+        self.die = False
+
+        QThread.__init__(self)
+
+    def __del__(self):
+        self.dispose()
+
+    def timeToDie(self):
+        print 'thread prepare to die'
+        self.die = True
+
+    def hex(self, symbol):
+        return '%02X' % ord(symbol)
+
+    def dispose(self):
+        print 'dispose'
+        if not self.disposed:
+            self.disposed = True
+            # закрываем порт
+            self.port.setDTR(False)
+            self.port.setRTS(False)
+            self.port.close()
+
+    def run(self):
+        if DEBUG:
+            # отладочный код, на случай отсутствия считывателя
+            print 'debugging part of code, in case of RFID '\
+                'reader absence. See rfid.py\'s DEBUG variable.'
+            demo_rfids = ['008365B0', '0083AD33', '00836012']
+            index = random.randint(0, len(demo_rfids) - 1)
+            rfid_code = demo_rfids[index]
+            time.sleep(1)
+            self.callback(rfid_code)
+            QCoreApplication.postEvent(self.dialog, QCloseEvent())
+            return
+
+        rfid_code = ''
+        # инициализация считывателя
+        self.port = serial.Serial(PORT['name'], PORT['rate'],
+                                  bytesize = PORT['bits_in_byte'],
+                                  parity = PORT['parity'],
+                                  stopbits = PORT['stop_bits']
+                             )
+        self.port.setDTR(True)
+        self.port.setRTS(True)
+
+        buffer = []
+        # бесконечный цикл, пока не получим идентификатор карты
+        # формат: =012345678<OD><OA>
+        while True:
+            symbol = self.port.read(1)
+            if not self.hex(symbol) == '0D':
+                buffer.append(symbol)
+            else:
+                if '0A' == self.hex(self.port.read(1)):
+                    if len(buffer) == 9:
+                        # первый символ нам не нужен
+                        rfid_code = ''.join(buffer[1:])
+                        break
+                    # в любом случае буфер надо чистить, так как в нём
+                    # либо уже ненужный идентификатор, либо тестовый
+                    # символ
+                    buffer = []
+
+                    # пользователь закрыл диалог, надо завершить поток
+                    if self.die:
+                        break
+
+        if not self.die:
+            # передаём код
+            self.callback(rfid_code)
+            # закрываем окно диалога
+            QCoreApplication.postEvent(self.dialog, QCloseEvent())
+
+        self.dispose()
