fd52c390ec93671806600be518dca7e2f72342ef
Ruslan <ruslan.popov@gmail.com>
2010-01-18 19:39:39 +0300

    Patch: It must be a float value.

--- patcher/patches/1001180955.sql
+++ patcher/patches/1001180955.sql
@@ -1,3 +1,3 @@
-alter table storage_schedule add column `duration` integer not null after begin;
-update storage_schedule set duration=60 where course_id in (select id from storage_course where duration=1);
-update storage_schedule set duration=90 where course_id in (select id from storage_course where duration=1.5);
+alter table storage_schedule add column `duration` float not null after begin;
+update storage_schedule set duration=1 where course_id in (select id from storage_course where duration=1);
+update storage_schedule set duration=1.5 where course_id in (select id from storage_course where duration=1.5);
