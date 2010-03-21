bd5fa8722517ba176e4619e5374b4ec1d0584f91
Ruslan Popov <ruslan.popov@gmail.com>
2010-03-21 11:44:58 +0300

    This data are not necessary any more because they were used for testing.

--- server/storage/sql/client.sql
+++
@@ -1,9 +0,0 @@
-insert into storage_client (first_name, last_name, email, rfid_code, reg_date) values 
-('Ruslan', 'Popov', 'radz@ya.ru', '008365B0', NOW()),
-('Vasily', 'Ivanov', 'ivanov@ya.ru', '96AF9B8B', NOW()),
-('Lebedev', 'Tema', 'tema@ya.ru', 'FCFC408E', NOW()),
-('Grigoriev', 'Alex', 'alex@ya.ru', '78DB28F8', NOW()),
-('Tinkov', 'Oleg', 'oleg@tinkoff.com', '0EAEEEA0', NOW()),
-('Kireev', 'Maxim', 'max@ya.ru', 'AE1CF55A', NOW()),
-('Seregin', 'Igor', 'iserega@ya.ru', '5E85D6AE', NOW()),
-('Peterkin', 'Nikolai', 'niko@ya.ru', 'F7988111', NOW());
--- server/storage/sql/room.sql
+++
@@ -1,4 +0,0 @@
-insert into storage_room (title, color) values
-('Красный зал', 'FFAAAA'),
-('Зелёный зал', 'AAFFAA'),
-('Синий зал', 'AAAAFF');
