0207f45d7759cd79badf6f576ce4a58b0531af5b
Ruslan Popov <ruslan.popov@gmail.com>
2010-10-01 20:01:36 +0400

    Share desc functionality on applications' pages.

--- server/storage/templatetags/model_desc.py
+++ server/storage/templatetags/model_desc.py
@@ -7,7 +7,10 @@ from django.db.models import get_model
 register = template.Library()
 
 @register.simple_tag
-def model_desc(url):
-    app_name = url.split('/')[0]
-    model_name = url.split('/')[1]
+def model_desc(url, app_name=None):
+    values = url.split('/')
+    if len(values) > 2: # project mode
+        model_name = values[1]
+    else: # application mode
+        model_name = values[0]
     return getattr(get_model(app_name, model_name), 'description', '')
