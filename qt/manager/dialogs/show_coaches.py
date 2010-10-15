ed26a81572b9794daa8dd3a864b9cdcd97afdf20
Ruslan Popov <ruslan.popov@gmail.com>
2010-10-15 19:26:34 +0400

    Shame on me, I've forgot this. This is the coaches list dialog.

---
+++ qt/manager/dialogs/show_coaches.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# (c) 2009-2010 Ruslan Popov <ruslan.popov@gmail.com>
+
+from settings import _, userRoles
+from ui_dialog import UiDlgTemplate
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+GET_ID_ROLE = userRoles['getObjectID']
+
+class ShowCoaches(UiDlgTemplate):
+
+    ui_file = 'uis/dlg_event_coaches.ui'
+    dialog = None
+    title = _('Registered visitors')
+    event_id = None
+
+    def __init__(self, parent=None, params=dict()):
+        UiDlgTemplate.__init__(self, parent, params)
+
+    def setupUi(self):
+        UiDlgTemplate.setupUi(self)
+
+        self.tableCoaches.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        self.connect(self.dialog.buttonApply, SIGNAL('clicked()'), self.apply)
+        self.connect(self.dialog.buttonClose,  SIGNAL('clicked()'), self, SLOT('reject()'))
+
+    def initData(self, schedule):
+        self.event_id = schedule.get('id', None)
+        for coach in self.parent.parent.static['coaches']:
+            lastRow = self.dialog.tableCoaches.rowCount()
+            self.dialog.tableCoaches.insertRow(lastRow)
+            name = QTableWidgetItem(coach['last_name']) # data may assign on cells only, use first one
+            name.setData(GET_ID_ROLE, int(coach['id']))
+            self.dialog.tableCoaches.setItem(lastRow, 0, name)
+            self.dialog.tableCoaches.setItem(lastRow, 1, QTableWidgetItem(coach['first_name']))
+            self.dialog.tableCoaches.setItem(lastRow, 2, QTableWidgetItem('--'))
+            self.dialog.tableCoaches.setItem(lastRow, 3, QTableWidgetItem(coach['reg_datetime']))
+
+    def apply(self):
+        coach_id_list = []
+        selected = self.tableCoaches.selectionModel().selectedRows()
+        if len(selected) > 3:
+            message = _('Select no more three coaches.')
+        else:
+            for index in selected:
+                model = index.model()
+                coach_id, ok = model.data(index, GET_ID_ROLE).toInt()
+                if ok:
+                    coach_id_list.append(coach_id)
+
+            if len(coach_id_list) > 0:
+                params = {'event_id': self.event_id, 'coach_id_list': ','.join([str(i) for i in coach_id_list])}
+                self.http.request('/manager/register_change/', params)
+                default_response = None
+                response = self.http.parse(default_response)
+                if response:
+                    message = _('Coaches have exchanged.')
+                else:
+                    message = _('Unable to exchange.')
+            else:
+                message = _('No selection, skip...')
+        QMessageBox.warning(self, _('Coaches exchange'), message)
