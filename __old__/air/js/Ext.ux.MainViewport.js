6a3ccfab3333c545015b6cf71897cf581d7f289e
Ruslan Popov <ruslan.popov@gmail.com>
2010-10-15 10:00:57 +0400

    Old code goes to trashbox.

--- __old__/air/js/Ext.ux.MainViewport.js
+++
@@ -1,8 +0,0 @@
-Ext.ux.MainViewport = Ext.extend(Ext.Viewport, {
-    layout: 'border',
-    title: 'Main window',
-    initComponent: function(){
-        Ext.ux.MainViewport.superclass.initComponent.call(this);
-    }
-});
-
--- __old__/client_air/js/URLS.js
+++
@@ -1,5 +0,0 @@
-if (typeof(URLS) == 'undefined') URLS = {};
-var root = 'http://127.0.0.1:8000';
-URLS.get_client_courses = root+'/client/ajax/active_courses/';
-URLS.get_user_data = root+'/ajax/rfid/';
-URLS.update_url = root+'/static/client_air/updater/update.xml';
