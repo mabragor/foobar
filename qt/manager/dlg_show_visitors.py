9723616d31a2397fa7feb2206ffc5d3cad044de4
Ruslan Popov <ruslan.popov@gmail.com>
2010-10-27 19:43:06 +0400

    Bug #132: Remove button for manual registration of a visit from visitors dialog.

--- qt/manager/dlg_show_visitors.py
+++ qt/manager/dlg_show_visitors.py
@@ -19,7 +19,6 @@ class ShowVisitors(UiDlgTemplate):
     def setupUi(self):
         UiDlgTemplate.setupUi(self)
 
-        self.connect(self.buttonManual, SIGNAL('clicked()'), self.registerManually)
         self.connect(self.buttonClose, SIGNAL('clicked()'), self, SLOT('reject()'))
 
 
@@ -36,19 +35,3 @@ class ShowVisitors(UiDlgTemplate):
             self.tableVisitors.setItem(lastRow, 1, QTableWidgetItem(first_name))
             self.tableVisitors.setItem(lastRow, 2, QTableWidgetItem(rfid_code))
             self.tableVisitors.setItem(lastRow, 3, QTableWidgetItem(reg_datetime))
-
-    def registerManually(self):
-        rfid_id, ok = QInputDialog.getText(self, _('Register manually'),
-                                           _('Enter a client\'s RFID code.'))
-        if ok:
-            if not rfid_id.isEmpty():
-                params = {'rfid_code': rfid_id,
-                          'event_id': self.event_id}
-                self.http.request('/manager/register_visit/', params)
-                default_response = None
-                response = self.http.parse(default_response)
-                if response:
-                    self.initData(self.event_id)
-            else:
-                QMessageBox.warning(self, _('Register manually'),
-                                    _('You\'ve entered empty string.'))
